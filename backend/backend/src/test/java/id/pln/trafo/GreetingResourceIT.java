5764bdd402f223ce067fd04f6bce46e1fb3e4187
Maulana-latifnabil <latifnabilmaulana@gmail.com>
2025-06-07 18:25:04 +0700

    Setup Quarkus project

---
+++ backend/backend/src/test/java/id/pln/trafo/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package id.pln.trafo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
